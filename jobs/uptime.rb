9c6d994e76030a6832ae17f955f7b97c9f89f56e
Martin Polden <martin.polden@gmail.com>
2013-02-09 12:31:55 +0100

    Implement Uptime widget

---
+++ jobs/uptime.rb
@@ -0,0 +1,17 @@
+require 'net/ping'
+require 'yaml'
+
+CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml'))
+DEVICES = CONFIG['uptime']['devices']
+
+PING = Net::Ping::External.new
+PING.timeout = 1
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+
+  DEVICES.each do |device|
+    device['active'] = PING.ping(device['host'])
+  end
+
+  send_event('uptime', { devices: DEVICES })
+end
