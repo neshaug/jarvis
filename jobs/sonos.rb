80b8d1b9d5b4728bd63ad1c9d543670ad78c6d15
Martin Polden <martin.polden@gmail.com>
2013-02-10 17:28:07 +0100

    Display Sonos room name

--- jobs/sonos.rb
+++ jobs/sonos.rb
@@ -3,10 +3,14 @@ require 'sonos'
 SONOS_DEVICE = Sonos::System.new.speakers.first
 
 SCHEDULER.every '10s', :first_in => 0 do |job|
-  # hack for disabling Savon logging
+  # hack that disables Savon logging
   savon_client = SONOS_DEVICE.instance_variable_get :@transport_client
   unless savon_client.nil?
     savon_client.globals[:log] = false
   end
-  send_event('sonos', SONOS_DEVICE.now_playing)
+  data = {
+    'name' => SONOS_DEVICE.name,
+    'np' => SONOS_DEVICE.now_playing
+  }
+  send_event('sonos', data)
 end
