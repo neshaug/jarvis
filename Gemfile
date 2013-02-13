7ca6d77d90e12f12a2c12577eeaed720aba64e03
Martin Polden <martin.polden@gmail.com>
2013-02-13 18:59:39 +0100

    Remove net-ping dep

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source :rubygems
 gem 'dashing'
 gem 'nokogiri'
 gem 'sonos'
-gem 'net-ping'
 gem 'google-api-client'
 gem 'activesupport'
--- jobs/uptime.rb
+++ jobs/uptime.rb
@@ -1,16 +1,13 @@
-require 'net/ping'
 require 'yaml'
 
 UPTIME_CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml'))
 DEVICES = UPTIME_CONFIG['uptime']['devices']
 
-PING = Net::Ping::External.new
-PING.timeout = 1
-
 SCHEDULER.every '1m', :first_in => 0 do |job|
 
   DEVICES.each do |device|
-    device['active'] = PING.ping(device['host'])
+    device['active'] = system('ping -c 1 -t 1 -q %s > /dev/null' %
+                              device['host'])
   end
 
   send_event('uptime', { devices: DEVICES })
