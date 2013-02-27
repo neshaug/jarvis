24b55501c1ad62b68b92594f768c24044a79d795
Martin Polden <martin.polden@gmail.com>
2013-02-27 13:59:52 +0100

    uptime: Redirect ping errors to /dev/null

--- jobs/uptime.rb
+++ jobs/uptime.rb
@@ -6,7 +6,7 @@ DEVICES = UPTIME_CONFIG['uptime']['devices']
 SCHEDULER.every '1m', :first_in => 0 do |job|
 
   DEVICES.each do |device|
-    device['active'] = system('ping -c 1 -t 1 -q %s > /dev/null' %
+    device['active'] = system('ping -c 1 -t 1 -q %s > /dev/null 2>&1' %
                               device['host'])
   end
 
